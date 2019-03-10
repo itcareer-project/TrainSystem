7a445ce681e3e4b9aac39d6778b57a24754fc415
Maria Shtereva <35622017+mariashtereva@users.noreply.github.com>
2019-03-10 12:52:33 +0200

    Add files via upload

---
+++ TrainSystemApp/cPrice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainSystemApp
+{
+    class cPrice
+    {
+        public double Sofiya = 45;
+        public double Bourgas = 45;
+        public double Varna = 45;
+        public double Plovdiv = 45;
+        public double GornaOrqhovica = 45;
+        public double Pleven = 45;
+        public double Blagoevgrad = 45;
+        public double Pernik = 45;
+
+        public double Acc_Single = 50;
+        public double Acc_Public = 50;
+        public double Acc_Extra = 50;
+
+        public double Std_Train = 2.5;
+        public double Firstclass_Train = 10;
+
+        public double Breakfast = 5.5;
+        public double AddLuggage = 10.5;
+        public double Alone = 3.5;
+    }
+}
